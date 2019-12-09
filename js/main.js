b28d40cd7575ea4a329894ad7509568a2c92faed
nataligm <nataligm89@gmail.com>
2019-12-09 22:25:52 +0200

    partners.carousel for showcase done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+    var partnerCarousel = $('.partners-carousel').owlCarousel({
+        loop: true,
+        nav: false,
+    });
+    $('.partner-slider .next').click(function() {
+        partnerCarousel.trigger('next.owl.carousel', [300]);
+    });
+    $('.partner-slider .prev').click(function() {
+        partnerCarousel.trigger('prev.owl.carousel', [300]);
+    });
+
     // $(document).on("click", ".scroll-to-top", function (e) {
     //     e.preventDefault();
     //     $("html, body").animate({ scrollTop: 0 }, 1000);
